1a01e0096e66c649039b41cc1ee6040e15a50648
Pushkarsuthar9521 <pushkar.suthar9521@gmail.com>
2023-09-14 16:28:09 +0530

    removed errors

--- index.js
+++ index.js
@@ -5,86 +5,138 @@ const prisma = new PrismaClient();
 const app = express(); // create express app
 app.use(express.json());
 
-app.get("/user", async (req, res) => {
-  
-  const users = await prisma.user.findMany();
-  res.send(users);
-});
-
-app.get("/user/:id", async (req, res) => {
-  
-  const user = await prisma.user.findMany({
-    where: { id: req.params.id },
-  });
-  res.send(user);
-});
-
+// create user
 app.post("/user", async (req, res) => {
-
-  const user = await prisma.user.create({
-    data: {
-      name: req.body.name,
-      email: req.body.email,
-    },
-  });
-  res.send(user);
-});
-
-app.patch("/user/:id", async (req, res) => {
   try {
     
-    const user = await prisma.user.update({
-      where: { id: req.params.id },
+    const user = await prisma.user.create({
       data: {
-        name: req.body.name,
-        email: req.body.email,
+        "name" : req.body.name,
+        "email" : req.body.email,
+        "password" : req.body.password,
+      }
+    });
+    res.json(user);
+  } catch (err) {
+    res.json(err);
+  }
+});
+// get user 
+app.get("/user", async (req, res) => {
+  try{
+    const user = await prisma.user.findMany();
+    res.json(user);
+  }catch(err){
+    res.json(err);
+  }
+});
+
+// delete user
+app.delete("/user/:id", async (req, res) => {
+  try{
+    const user = await prisma.user.delete({
+      where: {
+        id: parseInt(req.params.id),
       },
     });
-    res.send(user);
-  } catch (error) {
-    res.status(404).send({ error: "user not found" });
+    res.json(user);
+  }catch(err){
+    res.json(err);
   }
 });
 
-app.delete("/user/:id", async (req, res) => {
-  try {
-    
-    const user = await prisma.user.delete({
-      where: { id: req.params.id },
+//update user
+app.put("/user/:id", async (req, res) => {
+  try{
+    const user = await prisma.user.update({
+      where: {
+        id: parseInt(req.params.id),
+      },
+      data: {
+        "name" : req.body.name,
+        "email" : req.body.email,
+        "password" : req.body.password,
+      },
     });
-    res.send(user);
-  } catch (error) {
-    res.status(404).send({ error: "user not found" });
+    res.json(user);
+  }catch(err){
+    res.json(err);
   }
 });
 
-// booking
-app.get("/booking", async (req, res) => {
-  
-  const booking = await prisma.booking.findMany();
-  res.send(booking);
-});
-
-
-
+// create booking
 app.post("/booking/:id", async (req, res) => {
   try {
     
     const booking = await prisma.booking.create({
       data: {
-        num_of_seats: req.body.num_of_seats,
-        userId: req.body.userId, // Connect the booking to an existing user
-      },
+        "numOfSeats" : req.body.numOfSeats,
+        "userId" : parseInt(req.params.id),
+      }
     });
-    res.send(booking);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("An error occurred while creating the booking.");
-  } finally {
-    await prisma.$disconnect();
+    res.json(booking);
+  } catch (err) {
+    res.json(err)
   }
 });
 
-app.listen(3001, () => {
-  console.log("server started at 3000");
+//get booking
+app.get("/booking", async (req, res) => {
+  try{
+    const booking = await prisma.booking.findMany();
+    res.json(booking);
+  }catch(err){
+    res.json(err);
+  }
+});
+
+//delete booking
+app.delete("/booking/:id", async (req, res) => {
+  try{
+    const booking = await prisma.booking.delete({
+      where: {
+        id: parseInt(req.params.id),
+      },
+    });
+    res.json(booking);
+  }catch(err){
+    res.json(err);
+  }
+});
+
+//update booking
+app.put("/booking/:id", async (req, res) => {
+  try{
+    const booking = await prisma.booking.update({
+      where: {
+        id: parseInt(req.params.id),
+      },
+      data: {
+        "numOfSeats" : req.body.numOfSeats,
+      },
+    });
+    res.json(booking);
+  }catch(err){
+    res.json(err);
+  }
+});
+
+
+// get users with booking
+app.get("/user/booking", async (req, res) => {
+  try{
+    const user = await prisma.user.findMany({
+      include: {
+        booking: true,
+      },
+    });
+    res.json(user);
+  }catch(err){
+    res.json(err);
+  }
+});
+
+
+app.listen(3006, () => {
+  console.log("server started at 3002");
 });
